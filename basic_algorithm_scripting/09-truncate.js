a8f120fb50bdf0b88dee2355b610806c965bf33a
pwizla <pwizla+github@gmail.com>
2016-06-07 21:24:46 +0200

    Fix improper use of the three dots
    In the previous version, the "trunc" var (i.e. the "..." string)
    was added to the end of **every** truncated string, even though
    in some edges cases, the string given as an argument does not
    need to be truncated.
    This commit first check if the given string needs truncation:
    for instance, if the second argument 'num' is greater or equal
    to the length of the first argument 'str', there's no need to
    truncate.

--- basic_algorithm_scripting/09-truncate.js
+++ basic_algorithm_scripting/09-truncate.js
@@ -1,15 +1,16 @@
 function truncateString(str, num) {
 	var trunc = "...";
 	var index;	
-	if (num <= 3) {
-		index = num;
-	}	else {
-		index = num - 3;
+	if (num < str.length) {
+		if (num <= 3) {
+			index = num;
+		}	else {
+			index = num - 3;
+		}
+		str = str.substring(0, index);
+		str = str.concat(trunc);
 	}
-	str = str.substring(0, index);
-	str = str.concat(trunc);
-
 	console.log(str)
 }
 
-truncateString("A-tisket a-tasket A green and yellow basket", 11);
+truncateString("A-tisket a-tasket A green and yellow basket", "A-tisket a-tasket A green and yellow basket".length);
