8a544185c5ded68738e61c805693030f224c32ed
pwizla <pwizla+github@gmail.com>
2016-06-08 14:46:06 +0200

    Fix edge cases where len is not dividable by size
    The new code checks whether there will be remaining items after
    the slicing loop, and pushes these items to newArr.

--- basic_algorithm_scripting/10-chunkyMonkey.js
+++ basic_algorithm_scripting/10-chunkyMonkey.js
@@ -1,16 +1,33 @@
 function chunkArrayInGroups(arr, size) {
   // create another "big" array that will contain all new subarrays
   var newArr = [];
+  var len = arr.length; // store it since it's often accessed
   // slice the original array to get the first sub-array
   // create loop to automate slicing
-  for (var i = 0; i <= arr.length - size; i += size) {
+  /* need to take care of remaining items when len is
+  	not dividable by size */
+  /* check whether arr can be chunked properly 
+  	or if there will remain items */
+  // calculate how many remaining items there will be 
+  var rem = len % size;
+  // loop for automated slicing
+  for (var i = 0; i <= len - size; i += size) {
 	  newArr.push(arr.slice(i, i+size));
-	  console.log("newArr: " , newArr);
 	}
-  // push further sub-array into the new one
-  /* what about dealing with remainders? i.e. when original array 
-  		is comprised of 5 arrays and size is 2, for instance? */
-  return arr; /* apparently, slice is non-destructive and will create a shallow copy of arr
+	// when items remains, push them to newArr
+  if (rem != 0) {
+  	/* slice takes two arguments: begin and end
+  		 the 'begin' index is calculated by substracting 'rem' to 'len';
+  		 for instance, if the array 'is' composed of 8 items, and
+  		 is to be chunked in 3 sub-arrays of 2 items each i.e. 'size = 2',
+  		 'rem' equals 2 so len - rem equals 8 - 2 = 6, meaning the 
+  		 previous slices left us at index 6 of 'arr' ;
+  		 the 'end' index is omitted; when omitted, the slice goes to
+  		 the end of the array (i.e. 'len' in our case) */
+  	newArr.push(arr.slice(len - rem));
+  }
+	console.log("newArr: " , newArr);
+  return newArr; /* apparently, slice is non-destructive and will create a shallow copy of arr
 		see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/slice */
 }
 
