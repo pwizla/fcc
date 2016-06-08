e0337f153fdf3477ce7c7fa06cb997c2a808099b
pwizla <pwizla+github@gmail.com>
2016-06-08 16:34:11 +0200

    Clean args by converting everything to lower case

--- basic_algorithm_scripting/12-mutations.js
+++ basic_algorithm_scripting/12-mutations.js
@@ -3,9 +3,14 @@
 	For example, ["hello", "Hello"], should return true because all of the letters in the second string are present in the first, ignoring case.
 	The arguments ["hello", "hey"] should return false because the string "hello" does not contain a "y".
 	Lastly, ["Alien", "line"], should return true because all of the letters in "line" are present in "Alien".		*/
-	
+
 	function mutation(arr) {
-	  return arr;
+		// don't forget to ignore case, e.g. by converting all to lower case
+		for (var i = 0; i < arr.length; i++)	{
+			arr[i] = arr[i].toLowerCase();
+		}
+		console.log(arr);
+	  // return arr;
 	}
 
-	mutation(["hello", "hey"]);
+	mutation(["HElLo", "hey"]);
