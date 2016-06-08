21793e638a89944e09ba5fc0a6f1d6cc63dcfe0d
pwizla <pwizla+github@gmail.com>
2016-06-08 11:33:25 +0200

    Try to automate slicing with a for loop
    … but it does not work because my implementation is not good enough yet!
    Currently I'm just pushing sliced values to another array, but it only
    replicates the original one.
    What I need to do in further commits is to create sub arrays with sliced
    values and push *these sub-arrays* to "newArr".

--- basic_algorithm_scripting/10-chunkyMonkey.js
+++ basic_algorithm_scripting/10-chunkyMonkey.js
@@ -2,14 +2,16 @@ function chunkArrayInGroups(arr, size) {
   // create another "big" array that will contain all new subarrays
   var newArr = [];
   // slice the original array to get the first sub-array
-  newArr.push(arr.slice(0, 0+size));
-  console.log("newArr: " + newArr);
   // create loop to automate slicing
+  for (var i = 0; i <= arr.length - size; i += size) {
+	  newArr.push(arr.slice(i, i+size));
+	  console.log("newArr: " + newArr);
+	}
   // push further sub-array into the new one
   /* what about dealing with remainders? i.e. when original array 
   		is comprised of 5 arrays and size is 2, for instance? */
   return arr; /* apparently, slice is non-destructive and will create a shallow copy of arr
-  								see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/slice */
+		see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/slice */
 }
 
 chunkArrayInGroups(["a", "b", "c", "d"], 2);
