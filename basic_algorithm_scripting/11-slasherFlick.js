13ebd8ef4c62f6355f23e4d873d20358aae33e9c
pwizla <pwizla+github@gmail.com>
2016-06-08 16:05:15 +0200

    Fix apparent previous "fail" (wasn't one!)
    The problem lay in the fact that I renamed the function 'slasherFlick'
    instead of 'slasher'.
    This sounds as a dumb fail on the FCC part…
    If I replace the function name in *both* the function declaration
    and the call of the function, it should be OK.
    My code **is** technically correct. It just fails the tests because
    the test expect the function to be called 'slasher'…
    
    Note to future self: DON'T CHANGE THE FUNCTION NAME in FCC placeholder code.

--- basic_algorithm_scripting/11-slasherFlick.js
+++ basic_algorithm_scripting/11-slasherFlick.js
@@ -1,11 +1,14 @@
 /* Return the remaining elements of an array after chopping off n elements from the head.
 	The head means the beginning of the array, or the zeroth index. */
 
-function slasherFlick(arr, howMany) {
+function slasher(arr, howMany) {
 	/* 'chopping off n elements from the head' could be reworded as 
 		 'keeping the last "array.length - n"th elements';
-		  we'll use array.splice() to directly get these nth to last elements; */
-	console.log(arr.splice(howMany, arr.length - 2));
+		  we'll use array.splice() to directly get these nth to last elements; 
+		  array.splice requires a start argument; here it's set to index = howMany;
+		  the deleteCount 2nd argument can be omitted if our goal is to delete
+		  all elements through the end, which is what we want here */
+	return arr.slice(howMany);
 }
 
-slasherFlick([1, 2, 3, 4, 5], 2);
+slasher(["burgers", "fries", "shake"], 1);
