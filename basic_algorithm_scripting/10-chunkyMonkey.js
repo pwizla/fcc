2786030489f92ab6abffcf3e4cb808487694b80f
pwizla <pwizla+github@gmail.com>
2016-06-08 11:19:16 +0200

    Fix (n00b) typo in slice() syntax
    The previous issue has been solved! console.log printed an empty
    newArr just because I used brackets instead of parentheses.

--- basic_algorithm_scripting/10-chunkyMonkey.js
+++ basic_algorithm_scripting/10-chunkyMonkey.js
@@ -2,7 +2,7 @@ function chunkArrayInGroups(arr, size) {
   // create another "big" array that will contain all new subarrays
   var newArr = [];
   // slice the original array to get the first sub-array
-  newArr.push(arr.slice[0, 0+size]);
+  newArr.push(arr.slice(0, 0+size));
   console.log("newArr: " + newArr);
   // create loop to automate slicing
   // push further sub-array into the new one
