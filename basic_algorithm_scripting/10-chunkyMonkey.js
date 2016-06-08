26b885a687fc4bb67e9992c1b2f7c3bd2ee88def
pwizla <pwizla+github@gmail.com>
2016-06-08 10:50:54 +0200

    Add function structure layout in plain text

--- basic_algorithm_scripting/10-chunkyMonkey.js
+++ basic_algorithm_scripting/10-chunkyMonkey.js
@@ -1,6 +1,13 @@
 function chunkArrayInGroups(arr, size) {
-  // Break it up.
-  return arr;
+  // create another "big" array that will contain all new subarrays
+  // create a first sub-array
+  // slice the original array to get the first sub-array
+  // create loop to automate slicing
+  // push further sub-array into the new one
+  /* what about dealing with remainders? i.e. when original array 
+  		is comprised of 5 arrays and size is 2, for instance? */
+  return arr; /* apparently, slice is non-destructive and will create a shallow copy of arr
+  	see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/slice */
 }
 
 chunkArrayInGroups(["a", "b", "c", "d"], 2);
