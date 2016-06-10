ce53822d2f3817cc866f83b0e7128a64a20b8e70
pwizla <pwizla+github@gmail.com>
2016-06-10 22:08:32 +0200

    Commit the solution to the challenge
    That was simple enough. Simply filter out values that are booleans
    will do the trick.

--- basic_algorithm_scripting/13-falsyBouncer.js
+++ basic_algorithm_scripting/13-falsyBouncer.js
@@ -2,9 +2,9 @@
 Remove all falsy values from an array.
 Falsy values in JavaScript are false, null, 0, "", undefined, and NaN.
 */
-
 function bouncer(arr) {
   // Don't show a false ID to this bouncer.
+  arr = arr.filter(Boolean);
   return arr;
 }
 
