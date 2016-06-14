96e587802c409112ac9964e38ac8cbd8b5215234
pwizla <pwizla+github@gmail.com>
2016-06-14 15:58:56 +0200

    Extract array to be filtered and items to filter out
    Slowly putting everything in place (deconstructing the 'destroyer'
    function arguments, first) to approach a solution in further commits.

--- basic_algorithm_scripting/14-seekAndDestroy.js
+++ basic_algorithm_scripting/14-seekAndDestroy.js
@@ -1,6 +1,20 @@
+function toBeDestroyed() {
+	return 
+}
 
 function destroyer(arr) {
   // Remove all the values
+  // arguments[0] will be the initial array whose values have to be filtered out
+  // arguments[1+] will be values to be filtered out
+  // create a real array from the list of arguments of the destroyer function
+  var args = Array.from(arguments);
+  // remove the first argument since it's the array to be filtered out
+  // args.shift();
+  var arrayToFilter = args.shift();
+
+  console.log("args is:", args);
+  console.log("arrayToFilter is:", arrayToFilter);
+  arr = arrayToFilter.filter(toBeDestroyed);
   return arr;
 }
 
