7bab558497337105f9150198988b542b61aa8a7f
pwizla <pwizla+github@gmail.com>
2016-06-15 16:07:39 +0200

    Commit solution to the challenge!

--- basic_algorithm_scripting/15-whereDoIBelong.js
+++ basic_algorithm_scripting/15-whereDoIBelong.js
@@ -1,6 +1,16 @@
 function getIndexToIns(arr, num) {
   // Find my place in this sorted array.
-  return num;
+  // include num into the array 
+  arr.push(num);
+  // sort the array numerically
+  function compareNumbers(a, b) {
+  	return a - b;
+  }
+
+  arr = arr.sort(compareNumbers);
+  
+  // find where num is located in the sorted array
+  return arr.indexOf(num);
 }
 
-getIndexToIns([40, 60], 50);
+getIndexToIns([10, 20, 30, 40, 50], 30)
