e2b507af3282f90c37d78b40ce842615019dfd17
pwizla <pwizla+github@gmail.com>
2016-06-16 00:16:59 +0200

    Write script that pulls a Dikkenek quote from my Github

--- intermediate_front_end_projects/random_quote_machine/script.js
+++ intermediate_front_end_projects/random_quote_machine/script.js
@@ -1,9 +1,15 @@
 $(document).ready( function() {
-	$.getJSON("http://quotes.rest/qod.json?category=funny", function(json) {
-//		$(".quote").html(JSON.stringify(json));
-		var theQuote = json.contents.quotes[0].quote;
-		console.log('quote: ', theQuote);
+	console.log("MES COUILLES !");
+	$.getJSON("https://raw.githubusercontent.com/pwizla/fcc/master/intermediate_front_end_projects/random_quote_machine/quotes.json", function(json) {
+		// quote = json.quotes.quote[0];
+		console.log('json: ', json);
+		quote = json.contents.quotes[0].quote;
 
-		$(".quote").html(theQuote);
+		console.log("quote: ", quote);
+
+		$(".quote").html(quote);
+		// var theQuote = json.contents.quotes[0].quote;
+		// console.log('quote: ', theQuote);
+	console.log("MES COUILLES !");
 	});
 });
