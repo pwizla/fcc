147cdc82851dc875a0d2b3584ee76ff378e26bf4
pwizla <pwizla+github@gmail.com>
2016-06-16 09:39:19 +0200

    Clean up code

--- intermediate_front_end_projects/random_quote_machine/script.js
+++ intermediate_front_end_projects/random_quote_machine/script.js
@@ -1,15 +1,6 @@
 $(document).ready( function() {
-	console.log("MES COUILLES !");
 	$.getJSON("https://raw.githubusercontent.com/pwizla/fcc/master/intermediate_front_end_projects/random_quote_machine/quotes.json", function(json) {
-		// quote = json.quotes.quote[0];
-		console.log('json: ', json);
-		quote = json.contents.quotes[0].quote;
-
-		console.log("quote: ", quote);
-
+		quote = json.contents.quotes[1].quote;
 		$(".quote").html(quote);
-		// var theQuote = json.contents.quotes[0].quote;
-		// console.log('quote: ', theQuote);
-	console.log("MES COUILLES !");
 	});
 });
