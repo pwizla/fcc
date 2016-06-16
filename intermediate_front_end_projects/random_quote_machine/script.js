f1042ce3576ea4224ec858ddc7cab7c57d00df7f
pwizla <pwizla+github@gmail.com>
2016-06-16 11:22:20 +0200

    Prettify quote and prepare for next quotes to be added
    I added a .length console print because in the forthcoming days
    I will add the randomness feature, so I need to check how many
    quotes the json file includes.

--- intermediate_front_end_projects/random_quote_machine/script.js
+++ intermediate_front_end_projects/random_quote_machine/script.js
@@ -1,6 +1,7 @@
 $(document).ready( function() {
 	$.getJSON("https://raw.githubusercontent.com/pwizla/fcc/master/intermediate_front_end_projects/random_quote_machine/quotes.json", function(json) {
-		quote = json.contents.quotes[1].quote;
+		quote = '"' + json.contents.quotes[1].quote + '"';
+		console.log('length: ', json.contents.quotes.length);
 		$(".quote").html(quote);
 	});
 });
